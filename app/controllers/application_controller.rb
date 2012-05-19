c380ff814be293be5e8ad02f0ddac8af9a96c235
Andy Triggs <andy.triggs@gmail.com>
2012-05-19 19:22:48 +0100

    Added #find_constituencies with filter for all controller actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :find_constituencies
+
+  protected
+
+  def find_constituencies
+    @constituencies = Constituency.all
+  end
+  
 end
