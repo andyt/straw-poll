8645043facf7a80a61fc747efef44e16479e390e
Andy Triggs <andy.triggs@gmail.com>
2012-05-20 12:18:37 +0100

    Edited candidate model.

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -5,11 +5,24 @@ class Candidate
 
   belongs_to :constituency
   belongs_to :party
+  has_many :votes
 
   alias_method :to_s, :name
 
   def name_and_party
     "%s (%s)" % [name, party.name]
+  end
+
+  def share_of_vote
+    if constituency.vote_count > 0
+      "%0.1f%%" % (100 * votes.count / constituency.vote_count.to_f)
+    else
+      '-'
+    end
+  end
+
+  def vote_count
+    @vote_count ||= votes.count
   end
   
 end
